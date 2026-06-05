ccce7d9f8488353f47624dcd0a3261563fcca8fd
lynhamor <62983235+lynhamor@users.noreply.github.com>
2026-06-05 22:04:00 +0800

    Delete src directory

--- src/main/java/com/api/simulation/database/repository/NbaTeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.simulation.database.repository;
-
-import com.api.simulation.database.entity.NbaTeam;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NbaTeamRepository extends JpaRepository<NbaTeam, Long> {
-}
--- src/main/java/com/api/simulation/database/repository/NbaTeamStatsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.simulation.database.repository;
-
-import com.api.simulation.database.entity.NbaTeamStats;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NbaTeamStatsRepository extends JpaRepository<NbaTeamStats, Long> {
-}
