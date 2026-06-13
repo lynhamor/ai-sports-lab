befc01959a61209fe72644c6e49d2c01c8ca05d8
Lyndon <lyndonhamor.trycansoft@hotmail.com>
2026-06-13 15:27:57 +0800

    code cleanup

--- src/main/java/com/api/simulation/controller/NbaController.java
+++ src/main/java/com/api/simulation/controller/NbaController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,8 +50,7 @@ public class NbaController {
             )
     })
     @PostMapping("/team")
-    public ResponseEntity<Object> generateTeam() throws JsonProcessingException {
-
+    public ResponseEntity<Object> generateTeam(){
         return ResponseEntity.ok(nbaService.generateTeam());
     }
 
